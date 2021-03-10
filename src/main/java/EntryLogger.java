1fd6029937803d3ef40c281a152912e29d7505fc
Alonso Sánchez <alonsocucei@gmail.com>
2021-03-09 21:37:07 -0600

    Update EntryLogger.java

--- src/main/java/EntryLogger.java
+++ src/main/java/EntryLogger.java
@@ -5,21 +5,22 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
-import java.util.Collections;
 import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.Logger;
 
 /**
- * Imagine this logger is used to capture business events that are written to a file that is eventually fed into an ETL
- * pipeline in our data warehouse.  The events are transformed into metrics and KPIs that product owners use to
- * monitor the health of the business.  The code and pipeline are very old and rarely change.
+ * Imagine this logger is used to capture business events that are written to a
+ * file that is eventually fed into an ETL pipeline in our data warehouse. The
+ * events are transformed into metrics and KPIs that product owners use to
+ * monitor the health of the business. The code and pipeline are very old and
+ * rarely change.
  *
- * One day when looking at the application logs, you notice that the for loop below randomly throws
- * ConcurrentModificationExceptions and loses some events in the process.  What are your recommendations for fixing
- * the bug and improving this code?  How would you go about testing and releasing your changes to minimize any side
- * effects downstream in the data warehouse?
+ * One day when looking at the application logs, you notice that the for loop
+ * below randomly throws ConcurrentModificationExceptions and loses some events
+ * in the process. What are your recommendations for fixing the bug and
+ * improving this code? How would you go about testing and releasing your
+ * changes to minimize any side effects downstream in the data warehouse?
  */
 public class EntryLogger {
 
@@ -28,7 +29,8 @@ public class EntryLogger {
   private Thread loggerThread;
 
   // These objects contain information about business domains that metrics can be derived from
-  private List<Object> logEntries = Collections.synchronizedList(new LinkedList<>());
+  // Use concurrent collection to be thread-safe
+  private ConcurrentLinkedQueue<Object> logEntries = new ConcurrentLinkedQueue<>();
 
   private PrintWriter logWriter = null;
 
@@ -37,25 +39,28 @@ public class EntryLogger {
 
       while (true) {
         try {
-          List<Object> copy;
-          synchronized (logEntries) {
-            copy = logEntries;
-            logEntries = Collections.synchronizedList(new LinkedList<>());
-
-            // toggle to see more log detail
-            // SYSTEM_LOG.info(String.format("logging %d events", copy.size()));
+          
+          //Move this code outside the loop to save time
+          try {
+            logWriter = getLogWriter();
+          } catch (IOException ioe) {
+            SYSTEM_LOG.severe(String.format("Error on getting the logger: %s", ioe.getMessage()));
           }
-          if (!copy.isEmpty()) {
+          
+          /*
+            Instead of doing a copy, save the current size, then iterate that
+            number of times, getting always the first element, and removing it
+            at the same time.
+          */
+          int size = logEntries.size();
 
-            // this loop throws a ConcurrentModificationException in production
-
-            for (Object logEntry : copy) {
-              logWriter = getLogWriter();
-              logWriter.println(logEntry);
-              MetricsSingleton.getInstance().recordMetrics(logEntry);
-            }
-            logWriter.flush();
+          for (int i = 0; i < size; i++) {
+            Object logEntry = logEntries.poll();
+            logWriter.println(logEntry);
+            MetricsSingleton.getInstance().recordMetrics(logEntry);
           }
+
+          logWriter.flush();
           Thread.sleep(flushInterval);
 
         } catch (Throwable ex) {
@@ -78,24 +83,38 @@ public class EntryLogger {
     if (!logFile.exists()) {
       logFile.createNewFile();
     }
-    
+
     if (logWriter != null) {
       logWriter.close();
     }
     return new PrintWriter(new FileOutputStream(logFile, true));
   }
 
+  //Wrapper method to access counter in MetricsSingleton
+  public int getNumberOfLogs() {
+    return MetricsSingleton.getInstance().getCounter();
+  }
+  
   /**
-  * Throughout this codebase, singletons are commonly used for instance management.   MetricsSingleton doesn't do anything,
-  * but is a placeholder to represent coupling to a Singleton in EntryLogger.
-  */
+   * Throughout this codebase, singletons are commonly used for instance
+   * management. MetricsSingleton doesn't do anything, but is a placeholder to
+   * represent coupling to a Singleton in EntryLogger.
+   */
   static class MetricsSingleton {
+
     private int sillyCounter = 0;
     private static final Logger METRICS_LOG = Logger.getLogger(MetricsSingleton.class.getName());
 
     private static MetricsSingleton instance = new MetricsSingleton();
-    private MetricsSingleton() { }
 
+    private MetricsSingleton() {
+    }
+
+    //Add getter method to compare the number of logs.
+    public int getCounter() {
+      return sillyCounter;
+    }
+    
     static MetricsSingleton getInstance() {
       return instance;
     }
